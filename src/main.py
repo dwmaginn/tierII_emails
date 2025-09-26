12f3b4aecea49af4d61ce2fded789f00f184b624
Luke <lce1868@g.rit.edu>
2025-09-26 10:39:08 -0400

    sending html template emails

--- src/main.py
+++ src/main.py
@@ -1,31 +1,28 @@
-import utils.contact_parser as parser
+from utils.csv_reader import parse_contacts_from_csv
 import os
-
 from mailersend import MailerSendClient, EmailBuilder
 from dotenv import load_dotenv
+from utils.json_reader import load_email_config
 
 load_dotenv()
+config = load_email_config()
 
 def send_in_bulk():
     ms = MailerSendClient(os.getenv('TIERII_MAILERSEND_API_TOKEN'))
-    contacts = parser.parse_contacts_from_csv('data/test/testdata.csv')
-    emails = [
-        EmailBuilder() \
-            .from_email(os.getenv('TIERII_SENDER_EMAIL')) \
-            .to_many([{"email": contact['email'], "name": contact['contact_name']} for contact in contacts]) \
-            .subject('Test Email') \
-            .html('<p>Hello, World!</p>') \
-            .text('Hello, World!') \
-            .build()
-    ]
+    contacts = parse_contacts_from_csv('data/test/testdata.csv')
+    for contact in contacts:
+        # Replace {name} placeholder with the contact's first name using string replacement
+        html_content = config['html_content'].replace('{name}', contact['first_name']) if config['html_content'] else ""
         
-    response = ms.emails.send_bulk(emails)
-    
-    print(response)
-    
-def main():
+        email = EmailBuilder() \
+            .from_email(os.getenv('TIERII_SENDER_EMAIL')) \
+            .to_many([{"email": contact['email'], "name": contact['contact_name']}]) \
+            .subject(config['subject']) \
+            .html(html_content) \
+            .text(config['body'].format(name=contact['first_name'])) \
+            .build()
+        response = ms.emails.send(email)
+        print(response)
+
+if __name__ == "__main__":
     send_in_bulk()
-    
-    
-if __name__ == '__main__':
-    main()
